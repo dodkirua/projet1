31276b1efde649769a1633b09afcc9cb7d5e5060
Dodkirua <73292390+dodkirua@users.noreply.github.com>
2020-12-07 13:28:52 +0100

    responsive encours

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # projet1
+
+Je vous demande de créer un jeu simple de devinette de nombre.
+Le jeu choisit aléatoirement un nombre entre 1 et 100, puis il met le joueur au défi de le deviner en 10 tentatives maxi.
+À chaque tour, le joueur doit être informé s'il a deviné ou non le bon nombre — si ce n'est pas le cas, le jeu lui indique si son estimation est trop basse ou trop élevée.
+Le jeu doit également rappeler au joueur les nombres déjà proposés.
+Le jeu se termine quand le joueur a deviné le nombre mystère, ou s'il a épuisé ses 10 chances.
+
+Bonus : A la fin du jeu, le joueur a la possibilité de débuter une nouvelle partie.
+
+Attention : Je veux une maquette réalisée avec le logiciel de votre choix, le code doit être hébergé sur Github pages, vous devrez donc créer votre propre repository !
+Faites un effort au niveau de l’interface, la page HTML doit être stylée et agréable à regarder !
---
+++ js/script.js
@@ -0,0 +1,73 @@
+let value = valueRandom();
+let error = 0;
+let answer = document.getElementById("answer");
+let restart = document.getElementById("restart");
+let input = document.getElementById("value");
+console.log(value);
+
+input.value="";
+
+function valueRandom(){
+    return Math.trunc(Math.random()*100);
+}
+
+/**
+ *
+ * @param val
+ * @returns {string|boolean}
+ */
+function testValue (val){
+    if (val === value){
+        return true;
+    }
+    else if(val < value){
+        return "small";
+    }
+    else {
+        return "big";
+    }
+}
+
+document.getElementById("valid").addEventListener("click",function(){
+
+    let valTest = input.value;
+
+    if (input.valueAsNumber === "NaN"){
+        answer.innerHTML = "Ceci n'est pas un chiffre ou un nombre";
+    }
+    else {
+        valTest = Math.trunc(valTest);
+        let test = testValue(valTest);
+        if (test === true){
+            answer.innerHTML = "Félicitation la réponse : "+valTest+" est le bonne réponse";
+            restart.style.display = "block";
+        }
+        else {
+            error +=1;
+            if (error === 10){
+                answer.innerHTML = "Dommage vous avez perdu!"
+                restart.style.display = "block";
+            }
+            else if(test === "small"){
+                answer.innerHTML = "La valeur est plus grande"
+                document.getElementById("reminder"+error).innerHTML="Plus grand que "+valTest;
+            }
+            else{
+                answer.innerHTML = "La valeur est plus petite"
+                document.getElementById("reminder"+error).innerHTML="Plus petit que "+valTest;
+            }
+        }
+    }
+});
+
+document.getElementById("restart").addEventListener('click', function (){
+
+    for (let i=1 ; i <= error ; i++){
+        document.getElementById("reminder"+i).innerHTML="";
+    }
+    error = 0;
+    value = valueRandom();
+    restart.style.display = "none";
+    answer.innerHTML = "";
+    input.value="";
+})
